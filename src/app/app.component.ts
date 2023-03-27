56b16be578f6f76a012e3841f12b33a4a95fbd73
Oleksandr Poshtaruk <kievsash@ukr.net>
2023-03-27 14:27:28 +0300

    README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ToolbarService, LinkService, ImageService, HtmlEditorService } from '@syncfusion/ej2-angular-richtexteditor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService]
+
 })
 export class AppComponent {
   title = 'test-app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RichTextEditorModule } from '@syncfusion/ej2-angular-richtexteditor';
+import { AppRoutingModule } from './app-routing.module';
+import { TestComponent } from './testComponent/test.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RichTextEditorModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
